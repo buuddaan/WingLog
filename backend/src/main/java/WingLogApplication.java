20a5657833b59568a55a16e4ef7a1865132af3cd
buuddaan <elvirale3i@gmail.com>
2026-04-10 11:07:17 +0200

    add main application class

---
+++ backend/src/main/java/WingLogApplication.java
@@ -0,0 +1,11 @@
+package com.winglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WingLogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WingLogApplication.class, args);
+    }
+}
