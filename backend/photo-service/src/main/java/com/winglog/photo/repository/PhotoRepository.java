1b760ba829a3192b5771da8eba4ae53e162f060c
johma <johman89@hotmail.com>
2026-04-29 09:15:54 +0200

    Update repository

---
+++ backend/photo-service/src/main/java/com/winglog/photo/repository/PhotoRepository.java
@@ -0,0 +1,18 @@
+
+package com.winglog.photo.repository;
+
+import com.winglog.photo.model.BirdImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PhotoRepository extends JpaRepository<BirdImage, UUID> {
+
+    List<BirdImage> findByUSerId(UUID userId);
+
+    List<BirdImage> findByFolderId(UUID folderId);
+
+    void deleteByIdAndUserId(UUID id, UUID userId);
+
+}
