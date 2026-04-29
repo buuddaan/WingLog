737dd7a517a2afbec833cb1ef82b2a550a1833aa
emmawidholm <emma.wb@hotmail.com>
2026-04-29 10:16:22 +0200

    add BirdImageRepository to photo-Service with Joakim

---
+++ backend/photo-service/src/main/java/com/winglog/photo/repository/BirdImageRepository.java
@@ -0,0 +1,17 @@
+package com.winglog.photo.repository;
+
+import com.winglog.photo.model.BirdImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BirdImageRepository extends JpaRepository<BirdImage, UUID> {
+    List<BirdImage> findByUserId(UUID userId);
+
+    List<BirdImage> findByFolderNameAndUserId(String folderName, UUID userId);
+
+    List<BirdImage> findByUserIdAndFolderNameIsNull(UUID userId);
+
+
+}
