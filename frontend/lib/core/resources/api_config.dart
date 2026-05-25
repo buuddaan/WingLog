bd87ce33adaad9086959c2a2ccfd8d408414c6de
AxeKuc <axel.kuchler@gmail.com>
2026-05-25 10:04:56 +0200

    Ändrat image och API

--- frontend/lib/core/resources/api_config.dart
+++ frontend/lib/core/resources/api_config.dart
@@ -1,23 +1,10 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 
 class ApiConfig {
-  // Byt ut detta mot din dators aktuella IP-adress!, jag tror att det är denna vi kommer byta till servern sen
-  static const String _localIpAddress = '192.168.1.209';
+  // Byt ut detta mot den PUBLIKA IP-adressen till din SSH-server
+  static const String _serverIp = '123.45.67.89';
 
   static String get baseUrl {
-    // Webbläsaren (Chrome) körs på datorn och hittar din backend via localhost.
-    if (kIsWeb) {
-      return 'http://localhost:8080/gateway';
-    }
-    // Android-emulatorer har en inbyggd spärr och måste använda en special-adress
-    // (10.0.2.2) för att hitta värddatorns localhost.
-    else if (Platform.isAndroid) {
-      return 'http://10.0.2.2:8080/gateway';
-    }
-    // telefoner är egna datorer på nätverket och måste peka på din dators IP.
-    else {
-      return 'http://$_localIpAddress:8080/gateway';
-    }
+    // Nu behöver vi inga if-satser längre. Alla enheter når servern på samma sätt!
+    return 'http://$_serverIp:8080/gateway';
   }
 }
