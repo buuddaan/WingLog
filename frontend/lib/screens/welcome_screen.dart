f8c0c7515a464e4ad6cb79075ac3f83a6d3574fe
buuddaan <elvirale3i@gmail.com>
2026-06-03 16:28:09 +0200

    Now working Google Oauth but on port 3000 and direct to auth-service /EF, EW, DSK, JÖ

--- frontend/lib/screens/welcome_screen.dart
+++ frontend/lib/screens/welcome_screen.dart
@@ -113,7 +113,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 
   // --- HÄR ÄR DEN NYA GOOGLE-INLOGGNINGEN --- // Axel, denna ändras efter API config för att lösa telefon google?
-String get _googleLoginUrl => 'http://localhost:8080/gateway/oauth2/authorization/google';
+String get _googleLoginUrl => 'http://localhost:8081/gateway/oauth2/authorization/google';
 
   Future<void> _handleGoogleSignIn() async {
     final Uri googleLoginUri = Uri.parse(_googleLoginUrl);
